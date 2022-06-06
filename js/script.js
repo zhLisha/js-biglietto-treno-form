f9e5e2b1311855578182b5dfc4f0a2f030e0fe49
zhLisha <lisazhou1709@gmail.com>
2022-06-06 15:00:05 +0200

    section 1 layout done

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* 
+GENERARE UN BIGLIETTO DEL TRENO CON LE SEGUENTO INFO:
+1. Nomme Passeggero
+2. Offerta a sua disposizione
+3. Numero carrozza radom
+4. Codice Cp random
+5. Costo finale del biglietto
+*/
+
+// Leggere le info inserite dal cliente 
