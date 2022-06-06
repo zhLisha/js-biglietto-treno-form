9db41eb5e21d4ce5828368968bde93c2455f74e5
zhLisha <lisazhou1709@gmail.com>
2022-06-06 15:52:11 +0200

    price calculation done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /* 
-GENERARE UN BIGLIETTO DEL TRENO CON LE SEGUENTO INFO:
+- CALCOLARE LA TARIFFA BASE
+- CALCOLARE IL PREZZO CON LO SCONTO 
+- GENERARE UN BIGLIETTO DEL TRENO CON LE SEGUENTO INFO:
 1. Nomme Passeggero
 2. Offerta a sua disposizione
 3. Numero carrozza radom
@@ -7,4 +9,27 @@ GENERARE UN BIGLIETTO DEL TRENO CON LE SEGUENTO INFO:
 5. Costo finale del biglietto
 */
 
-// Leggere le info inserite dal cliente 
+
+// Generare biglietto
+let submitButton = document.getElementById('button_submit');
+submitButton.addEventListener ( 'click',
+    function() {
+        // Lettura delle informazioni inserite dal cliente
+        const userName = document.getElementById('user-name').value;
+        const userKm = document.getElementById('user-km').value;
+        const userAge = document.getElementById('user-age').value;
+
+        // Calcolo prezzofisso
+        const fixedPrice = 0.21;
+        let standardPrice = userKm * fixedPrice;
+
+        // Calcolo disconto e applicazione
+        if(userAge === 'minorenne') {
+            discountPrice = standardPrice - (standardPrice * 20 / 100);
+        } else if(userAge === 'over65') {
+            discountPrice = standardPrice - (standardPrice * 40 / 100)
+        } else {
+            discountPrice = standardPrice
+        }
+    }
+)
